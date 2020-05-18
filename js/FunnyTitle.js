4a23686aab90ccc719f29c5fcec0fc39908c9b77
cher112 <961162300@qq.com>
2020-05-18 14:21:14 +0800

    Site updated: 2020-05-18 14:21:10

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,12 +3,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         $('[rel="icon"]').attr('href', "/img/favicon.png");
          document.title = '😒哼赶紧走,不要回来了！';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         $('[rel="icon"]').attr('href', "/img/favicon.png");
          document.title = '😳不是因为你回来才开心哦！';
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
